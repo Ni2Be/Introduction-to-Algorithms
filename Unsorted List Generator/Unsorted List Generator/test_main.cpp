de10f60c2eaf2d79848ff305065920d1279660fb
Nice2BeeDesktop <nice2beeg@gmail.com>
2018-02-21 16:11:40 +0100

    random sequence generator

---
+++ Unsorted List Generator/Unsorted List Generator/test_main.cpp
@@ -0,0 +1,31 @@
+//#include <iostream>
+//#include <vector>
+//#include <deque>
+//
+//#include "Sequence_Generator.h"
+//
+//int main()
+//{
+//	using namespace N2B_Algo;
+//	Random_Int_Sequence_Generator<std::vector<int>> random(10, -5, 5);
+//
+//
+//	std::cout << "Random_Real_Sequence_Generator:" << std::endl;
+//	for (auto i : random.numbers)
+//	{
+//		std::cout << i << ", ";
+//	}
+//	std::cout << std::endl << std::endl;
+//
+//	std::cout << "Unsorted_Sequence_Generator:" << std::endl;
+//	Unsorted_Sequence_Generator<std::vector<double>> unsorted(10, -5, 0.2);
+//	for (auto i : unsorted.numbers)
+//	{
+//		std::cout << i << ", ";
+//	}
+//	std::cout << std::endl;
+//
+//
+//	char ch;
+//	std::cin >> ch;
+//}
